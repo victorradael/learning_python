fb69d0fd952b454d2f3d120eacaf17defeb66160
Victor Radael <victorradael@hotmail.com>
2020-12-31 15:28:02 -0300

    :snake: Booleans and Sets

---
+++ setsAndBooleans.py
@@ -0,0 +1,17 @@
+x = set()
+
+x.add(1)
+print(x)
+
+x.add('one')
+print(x)
+
+lista = [11, 11, 11, 22, 22, 3, 4, 1, 2, 2]
+
+set_lista = set(lista)
+print(set_lista)
+
+#Booleanos
+
+a = True
+b = False
