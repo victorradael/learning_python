bb2111f1114f06f524e5cfefed402bc728b61255
Victor Radael <victorradael@hotmail.com>
2021-01-04 22:20:55 -0300

    :snake: Declarations

---
+++ declarations.py
@@ -0,0 +1,25 @@
+#If em Python
+
+if 1 < 2:
+  print('Deu Certo')
+
+#identação define a organização.
+
+#if, elif e else
+
+if 1 < 2:
+  print('true')
+
+#elif
+
+if 1 > 2:
+  print('true')
+elif 1 < 3
+  print('true') # ele segue conferindo de acordo com o que vem após o if no elif
+
+# else
+
+if 1 > 2:
+  print('true')
+else: 
+  print('true')
