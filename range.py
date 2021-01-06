29375cf809b386a5c94d8e86d837f0def53ef99b
Victor Radael <victorradael@hotmail.com>
2021-01-06 20:44:49 -0300

    :snake: range()

---
+++ range.py
@@ -0,0 +1,15 @@
+varRange = range(0, 10, 1)
+print(varRange)
+varList = list(varRange)
+print(varList)
+
+varRange = range(0, 50, 3)
+print(varRange)
+varList = list(varRange)
+print(varList)
+
+
+for i in range(0, 100):
+  print(i)
+  if i > 10:
+    break
