58eab320b07559fbd1e2f7f4ffd84a40f4ca84b7
Victor Radael <victorradael@hotmail.com>
2021-01-06 21:03:51 -0300

    :snake: Functions

--- functions.py
+++ functions.py
@@ -13,3 +13,19 @@ def soma(num1, num2):
   return num1 + num2
 
 print(soma(1,2))
+
+def primo(num):
+  """
+  Metodo para checar se eh primo
+  """
+
+  for n in range(2, num):
+    if num % n == 0:
+      print('Nao eh primo')
+      break
+    else:
+      print('Primo')
+      break
+
+primo(11)
+primo(120)
