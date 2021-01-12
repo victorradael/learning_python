f8e85422ef0f03c2f9eda06aed640ee2c9074426
Victor Radael <victorradael@hotmail.com>
2021-01-11 23:36:54 -0300

    :snake: Lambda Functions

---
+++ lambda_function.py
@@ -0,0 +1,17 @@
+def square(num):
+  result = num ** 2
+  return result
+
+print(square(2))
+
+def square(num):  #def square(num): return num ** 2
+  return num ** 2
+   
+
+print(square(3))
+
+# Funcao Lambda
+
+square = lambda num: num ** 2
+
+print(square(4))
